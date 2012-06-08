1126621d9afbd0006db3e85be8fe9f304dc5a9c5
Heiko W. Rupp <hwr@pilhuhn.de>
2012-06-08 18:05:00 +0200

    Fragment to display user details

---
+++ src/org/rhq/core/domain/rest/UserRest.java
@@ -0,0 +1,68 @@
+
+package org.rhq.core.domain.rest;
+
+
+/**
+ * A user
+ * @author Heiko W. Rupp
+ */
+public class UserRest {
+
+    int id;
+    String login;
+    String firstName;
+    String lastName;
+    String tel;
+    String email;
+
+    public UserRest() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ src/org/rhq/pocket/UserDetailFragment.java
@@ -0,0 +1,84 @@
+package org.rhq.pocket;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.DeserializationConfig;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
+
+import org.rhq.core.domain.rest.UserRest;
+
+/**
+ * Display details of a user
+ * @author Heiko W. Rupp
+ */
+public class UserDetailFragment extends Fragment {
+
+    private TableLayout tableLayout;
+    private TextView loginView;
+    private TextView firstNameView;
+    private TextView lastNameView;
+    private TextView emailView;
+    private TextView phoneView;
+    private String login;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        tableLayout = (TableLayout) inflater.inflate(R.layout.user_detail_fragment,container,false);
+        loginView = (TextView) tableLayout.findViewById(R.id.user_detail_login);
+        firstNameView = (TextView) tableLayout.findViewById(R.id.user_detail_firstName);
+        lastNameView = (TextView) tableLayout.findViewById(R.id.user_detail_lastName);
+        emailView = (TextView) tableLayout.findViewById(R.id.user_detail_email);
+        phoneView = (TextView) tableLayout.findViewById(R.id.user_detail_phone);
+        return tableLayout;
+    }
+
+    public void setLogin(String login) {
+
+        this.login = login;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        new TalkToServerTask(getActivity(),new FinishCallback() {
+            @Override
+            public void onSuccess(JsonNode result) {
+                ObjectMapper objectMapper = new ObjectMapper();
+                objectMapper.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+                try {
+                    UserRest user = objectMapper.readValue(result,new TypeReference<UserRest>() {});
+                    fillFields(user);
+                }
+                catch (Exception e) {
+                    e.printStackTrace(); // TODO
+                }
+
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                Toast.makeText(getActivity(),"Getting user info failed: " + e.getLocalizedMessage(),Toast.LENGTH_LONG).show();
+            }
+        },"/user/" + login).execute();
+    }
+
+    private void fillFields(UserRest user) {
+        loginView.setText(user.getLogin());
+        firstNameView.setText(user.getFirstName());
+        lastNameView.setText(user.getLastName());
+        emailView.setText(user.getEmail());
+        phoneView.setText(user.getTel());
+    }
+}
